7d1c94cafa19a1eada0e284674de755fc3e92ac6
Chaal Pritam <chaalpritam@gmail.com>
2024-04-22 01:54:32 +0530

    update to make center

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div className="min-h-screen flex justify-center items-center">
-      <h3 className="text-center">Carbide</h3><br />
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <h3 className="text-center">Carbide</h3>
       <p className="text-center">redefining the consumer storage space</p>
-  </div>
+    </div>
   );
 }
