fd05870a099a85d6657e4864683c67754f107507
Chaal Pritam <chaalpritam@gmail.com>
2024-04-22 02:13:57 +0530

    changes to bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
+    <div className="min-h-screen flex flex-col justify-center items-center bg-black">
       <Image width="240" height="80" src="/logo.png" alt="Carbide Logo" className="max-w-full" />
       <p className="text-center">redefining the consumer storage space</p>
     </div>
