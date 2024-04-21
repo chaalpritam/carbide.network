7af76bd20ac82f5d52aa412fed593d3167a9b7da
Chaal Pritam <chaalpritam@gmail.com>
2024-04-22 01:46:31 +0530

    next line typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="min-h-screen flex justify-center items-center">
-      <h3 className="text-center">Carbide</h3>
+      <h3 className="text-center">Carbide</h3><br />
       <p className="text-center">redefining the consumer storage space</p>
   </div>
   );
