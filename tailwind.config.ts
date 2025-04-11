c527ac7fecbc8f18699356f6dbc2ce8d13016d63
Chaal Pritam <chaalpritam@gmail.com>
2025-04-12 00:36:04 +0530

    fixing tailwind config to make page load css properly

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,13 +3,10 @@ import type { Config } from "tailwindcss"
 const config = {
     darkMode: "class",
     content: [
-        "./pages/**/*.{ts,tsx}",
-        "./components/**/*.{ts,tsx}",
-        "./app/**/*.{ts,tsx}",
-        "./src/**/*.{ts,tsx}",
-        "*.{js,ts,jsx,tsx,mdx}",
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
-    prefix: "",
     theme: {
         container: {
             center: true,
